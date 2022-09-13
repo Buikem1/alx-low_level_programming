6a89f80154bef640f13847214e1ec6be64b73e1a
Buikem <nwobodochukwubuikem@gmail.com>
2022-09-13 13:16:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+  * main - check the code 
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+	char ab;
+
+	ab = 'a';
+
+	while (ab <= 'z')
+	{
+		_putchar(ab);
+		ab++;
+	}
+        _putchar('\n');
+}
