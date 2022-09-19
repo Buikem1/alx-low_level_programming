6a7e5b104434ea7133a66b450d1cdfaa01bc6c45
Buikem <nwobodochukwubuikem@gmail.com>
2022-09-19 10:36:19 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -12,13 +12,7 @@ void swap_int(int *a, int *b)
 {
 	int temp;
 
-<<<<<<< HEAD
 	temp=*a;
         *a=*b;
         *b=temp;
-=======
-        temp = *a;
-        *a = *b;
-        *b = temp;
->>>>>>> a3121b53d4c418d0d7ec20b2ae41a06e65600162
 }
