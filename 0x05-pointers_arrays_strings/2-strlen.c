0a72cb58997cc1458ff46853bf2de0e4d2689a8e
1Buike <105224708+Buikem1@users.noreply.github.com>
2022-09-19 20:20:41 +0100

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,19 @@
 #include "main.h" 
-/** 
-* _strlen - Returns the length of a string 
-* @s: String to count 
-* 
-* Return: String length 
-*/
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string parameter input
+ *
+ * Return: length of string
+ */
 
 int _strlen(char *s)
 {
-	int c = 0; 	
-for (; *s != '\0'; s++)
-	{
-		c++;	
-} 	
-return (c);
+	int counter;
+
+	for (counter = 0; *s != '\0'; ++s)
+		++counter;
+
+	return (counter);
 }
