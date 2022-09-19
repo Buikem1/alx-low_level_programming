6e55f634f43998aa12dc64eecdd3b51f1841ea1a
Buikem <nwobodochukwubuikem@gmail.com>
2022-09-19 03:32:46 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * writes a function that takes apointer to an int as parameterand updates the value it point to 98.
+ *
+ * prototype void reset_to_98(int *n);
+ *
+ *
+ */
+
+ void reset_to_98(int *n)
+{
+	*n = 98;
+}
